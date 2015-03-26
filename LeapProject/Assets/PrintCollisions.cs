73f455ed9818737fd23c15e3c4ff94af6d6d3c6b
kal5544 <kal5544@psu.edu>
2015-03-26 16:21:22 -0400

    PrintCollisions is now printing data to console

--- LeapProject/Assets/PrintCollisions.cs
+++ LeapProject/Assets/PrintCollisions.cs
@@ -14,7 +14,7 @@ public class PrintCollisions : MonoBehaviour {
 	void OnCollisionStay(Collision coll)
 	{
 		debugString = gameObject.name + " in contact with " + coll.gameObject.name + " with force: " + Vector3.Dot (coll.contacts[0].normal, coll.relativeVelocity)*coll.rigidbody.mass;
-		//Debug.Log (debugString);
+		Debug.Log (debugString);
 	}
 
 }
