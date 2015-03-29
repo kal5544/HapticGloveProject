b07590d9302b90fd5d84422488538f45c9af4953
kal5544 <kal5544@psu.edu>
2015-03-29 16:44:29 -0400

    Moved data sending from SendData to Print Collisions

--- LeapProject/Assets/PrintCollisions.cs
+++ LeapProject/Assets/PrintCollisions.cs
@@ -1,14 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO.Ports;
+using System.Threading;
 
 public class PrintCollisions : MonoBehaviour {
 
+	//Setup parameters to connect to Arduino
+	public static SerialPort sp = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One);
+	public int motorPin = 2;
+
 	private string debugString = "";
+
+	private byte[] motorOn = new byte[]{2, 255};
+
+	void Start()
+	{
+		OpenConnection ();
+		motorOn [0] = (byte)motorPin;
+	}
 
 	void OnCollisionEnter(Collision coll)
 	{
-		Debug.Log ("modifying mesh");
-		GameObject.Find ("HandControllerSandBox").GetComponent<MeshFunctionality> ().ModifyMesh (coll);
+		//Debug.Log ("modifying mesh");
+		//GameObject.Find ("HandControllerSandBox").GetComponent<MeshFunctionality> ().ModifyMesh (coll);
+		motorOn [1] = (byte)255;
+		Debug.Log ("Motor Magnitude: " + (int)motorOn [1]);
+		sp.Write (motorOn, 0, 2);
+
+	}
+
+	void OnCollisionExit(Collision coll)
+	{
+		motorOn [1] = (byte)0;
+		Debug.Log ("Motor Magnitude: " + (int)motorOn [1]);
+		sp.Write (motorOn, 0, 2);
+
 	}
 
 	void OnCollisionStay(Collision coll)
@@ -16,5 +42,37 @@ public class PrintCollisions : MonoBehaviour {
 		debugString = gameObject.name + " in contact with " + coll.gameObject.name + " with force: " + Vector3.Dot (coll.contacts[0].normal, coll.relativeVelocity)*coll.rigidbody.mass;
 		Debug.Log (debugString);
 	}
+
+
+	//Function connecting to Arduino
+	public void OpenConnection() 
+	{
+		if (sp != null) 
+		{
+			if (sp.IsOpen) 
+			{
+				sp.Close();
+				Debug.Log("Closing port, because it was already open!");
+			}
+			else 
+			{
+				sp.Open();  // opens the connection
+				sp.ReadTimeout = 50;  // sets the timeout value before reporting error
+				Debug.Log("Port Opened!");
+			}
+		}
+		else 
+		{
+			if (sp.IsOpen)
+			{
+				Debug.Log("Port is already open");
+			}
+			else 
+			{
+				Debug.Log("Port == null");
+			}
+		}
+	}
+
 
 }
